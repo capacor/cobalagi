6821d51fe6eda67079b2f4485e9f1c660d807026
capacor <nurizzatiqumillaila@gmail.com>
2024-01-23 10:09:58 +0700

    bismillah

---
+++ db/migrate/20240122162435_create_gurus.rb
@@ -0,0 +1,10 @@
+class CreateGurus < ActiveRecord::Migration[7.1]
+  def change
+    create_table :gurus do |t|
+      t.string :nama
+      t.string :bid_studi
+
+      t.timestamps
+    end
+  end
+end
